34ca679ca41f3a7d06acc6307c27fec501acdebf
Luis Chavez <luis.chavez04@utrgv.edu>
2026-03-02 21:51:07 -0600

    initial commit

---
+++ Player.cs
@@ -0,0 +1,54 @@
+using Godot;
+
+public partial class Player : CharacterBody3D
+{
+	// player movement in meters per second
+	[Export]
+	public int Speed {get; set; } = 14;
+	// The downward acceleration when in the air, in meters per second squared.
+	[Export]
+	public int FallAcceleration { get; set; } = 75;
+	
+	private Vector3 _targetVelocity = Vector3.Zero;
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		// local var for input direction
+		var direction = Vector3.Zero;
+		
+		if (Input.IsActionPressed("move_right")){
+			direction.X += 1.0f;
+		}
+		
+		if (Input.IsActionPressed("move_left")){
+			direction.X -= 1.0f;
+		}
+		
+		if (Input.IsActionPressed("move_back")){
+			direction.Z += 1.0f;
+		}
+		
+		if (Input.IsActionPressed("move_forward")){
+			direction.Z -= 1.0f;
+		}
+		
+		if (direction != Vector3.Zero){
+			direction = direction.Normalized();
+			// Setting the basis property wil affect the rotation of the node.
+			GetNode<Node3D>("Pivot").Basis = Basis.LookingAt(direction);
+		}
+		
+		// Ground velocity
+		_targetVelocity.X = direction.X * Speed;
+		_targetVelocity.Z = direction.Z * Speed;
+		
+		// Vertical velocity
+		if (!IsOnFloor()){
+			_targetVelocity.Y -= FallAcceleration * (float)delta;
+		}
+		
+		// Moving the character
+		Velocity = _targetVelocity;
+		MoveAndSlide();
+	}
+}
