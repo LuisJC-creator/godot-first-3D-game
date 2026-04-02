61d904110e8bd0030d0bbbe553fb7830085cfe20
Luis Chavez <luis.chavez04@utrgv.edu>
2026-04-02 10:53:11 -0500

    stuck connected signal to mob function not appearing.

--- Mob.cs
+++ Mob.cs
@@ -3,5 +3,37 @@ using System;
 
 public partial class Mob : CharacterBody3D
 {
+	// Minimum speed of the mob in meters per second
+	[Export]
+	public int MinSpeed {get; set } = 10;
 	
-}
+	// max speed of mob in meters per second
+	[Export]
+	public int MaxSpeed
+	
+	public override void _PhysicsProcess(double delta){s
+		MoveandSlide();
+	}
+	
+	public void initialize(Vector3 startPosition, Vector3 playerPosition){
+		
+		// mob starts at startPos
+		// using playPos, it will rotate toward player
+		LookAtFromPosition(startPosition, playerPosition, Vector3.Up);
+		// rotate the mob in predefined range
+		// dont want it to move straight at the player ( though that could be fun )
+		RotateY((float)GD.RandRange(-Mathf.Pi / 4.0, Mathf.Pi / 4.0));
+		
+		// random speed
+		int randomSpeed = GD.RandRange(MinSpeed, MaxSpeed);
+		// fix velocity
+		Velocity = Vector3.Forward * randomSpeed;
+		// fix rotation
+		Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y);
+	}
+	
+	private void OnVisibilityNotifierScreenExited(){
+		QueueFree();
+	}
+	
+	
